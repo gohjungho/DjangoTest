796c4cd2d0053dbd00114c80f85fbcb10544f691
Jetty <jerry940808@gmail.com>
2021-04-04 18:28:17 +0900

    Modified templates to display posts from database.

--- lottery/views.py
+++ lottery/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Lottery
 
 def lottery_list(request):
-    return render(request, 'lottery/lottery_list.html', {})
+    posts = Lottery.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'lottery/lottery_list.html', {'posts':posts})
+
