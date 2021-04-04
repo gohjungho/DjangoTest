2cf6b022714e0c4d8c099d7e2ea586c5c8871997
Jetty <jerry940808@gmail.com>
2021-04-04 19:25:59 +0900

    upload

--- lottery/urls.py
+++ lottery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lottery_list, name='lottery_list'),
+    path('lottery/<int:pk>/', views.lottery_detail, name='lottery_detail'),
 ]
--- lottery/views.py
+++ lottery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Lottery
 
@@ -6,3 +6,6 @@ def lottery_list(request):
     posts = Lottery.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'lottery/lottery_list.html', {'posts':posts})
 
+def lottery_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'lottery/lottery_detail.html', {'post': post})
